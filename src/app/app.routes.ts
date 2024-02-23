e06ac0f71d0803fca8a51165771abf2ad7661184
Pedro Lorenzo <plorenzo2184@intersg.edu>
2024-02-23 14:34:29 -0400

    dfd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,14 @@ import { ContactComponent } from './pages/contact/contact/contact.component';
 import { AuthorComponent } from './pages/author/author/author.component';
 import { AboutComponent } from './pages/about/about/about.component';
 import { NotFoundComponent } from './pages/page-notfound/not-found/not-found.component';
+import { SearchCatContentComponent } from './pages/search-cat/search-cat-content/search-cat-content.component';
+import { SearchComponent } from './pages/search-cat/search/search.component';
 
 export const routes: Routes = [
     {path:'', component: HomeComponent},
     {path: 'home', component: HomeComponent},
     {path:'signUp', component:SignUpComponent},
+    {path: 'search/:cat', component: SearchComponent},
     {path: 'privacy', component: PrivacyComponent},
     {path: 'faq', component: FaqComponent},
     {path: 'contact', component: ContactComponent},
